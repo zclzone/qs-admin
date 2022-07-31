2751273f11225b10f4f47adca30b2344a509336f
张传龙 <zclzone@outlook.com>
2022-07-31 16:53:50 +0800

    style: style adjust

--- src/layout/header/components/FullScreen.vue
+++ src/layout/header/components/FullScreen.vue
@@ -6,7 +6,7 @@ const { isFullscreen, toggle } = useFullscreen()
 
 <template>
   <n-icon mr-20 cursor-pointer size="18" @click="toggle">
-    <icon-ant-design:fullscreen-exit-outlined v-if="isFullscreen" color-red />
+    <icon-ant-design:fullscreen-exit-outlined v-if="isFullscreen" />
     <icon-ant-design:fullscreen-outlined v-else />
   </n-icon>
 </template>
