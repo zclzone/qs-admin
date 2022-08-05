5b6cb6d75e561028e65d05f2f0d9e378505bd896
张传龙 <zclzone@outlook.com>
2022-08-05 09:51:02 +0800

    fix: page title

--- src/router/guard/page-title-guard.ts
+++ src/router/guard/page-title-guard.ts
@@ -4,7 +4,7 @@ const baseTitle: string = import.meta.env.VITE_APP_TITLE
 
 export function createPageTitleGuard(router: Router) {
   router.afterEach((to) => {
-    const pageTitle = to.meta?.itle
+    const pageTitle = to.meta?.title
     if (pageTitle)
       document.title = `${pageTitle} | ${baseTitle}`
     else
